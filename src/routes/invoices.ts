b33e3a29e723309cb18ecc96ffd49d7eac8bd870
Agus <aguscm23@gmail.com>
2024-04-06 00:08:46 +0200

    Modify get invoices endpoint

--- src/routes/invoices.ts
+++ src/routes/invoices.ts
@@ -1,17 +1,34 @@
 import express from "express";
-import type { IInvoice } from "../interfaces";
+import type { IInvoice, IInvoiceShort, IProduct, IClient, IResponse } from "../interfaces";
 import requireLogin from "../middlewares/requireLogin";
 const allInvoices = require("../data/invoices.json");
+const allProducts = require("../data/products.json");
+const allClients = require("../data/clients.json");
 
 const router = express.Router();
 
-let invoiceList: IInvoice[] = allInvoices;
+let invoiceList: IInvoiceShort[] = allInvoices;
+let productList: IProduct[] = allProducts;
+let clientList: IClient[] = allClients;
+let invoiceFullList: IInvoice[] = invoiceList.map((invoice: IInvoiceShort) => {
+    const product = productList.find((p: IProduct) => p.id === invoice.idProduct);
+    const client = clientList.find((c: IClient) => c.id === invoice.idClient);
 
+    delete invoice.idProduct;
+    delete invoice.idClient;
+
+    return {
+      ...invoice,
+      product,
+      client,
+    };
+});
 router.route("/").get(requireLogin, function (req, res) {
-  let response = {
+  let response: IResponse;
+  response = {
     error: false,
     code: 200,
-    data: invoiceList,
+    data: invoiceFullList,
   };
 
   res.status(response.code).send(response);;
